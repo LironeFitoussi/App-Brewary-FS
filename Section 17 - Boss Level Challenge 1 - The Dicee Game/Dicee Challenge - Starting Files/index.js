c18d89254495238277348f9b05bc473d58143e26
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2023-07-23 14:19:26 +0300

    upload dice chalenge

---
+++ Section 17 - Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,17 @@
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    
+    var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+    var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+    
+    var image1 = document.getElementsByClassName("img1")[0].setAttribute("src", randomDiceImage1);
+    var image2 = document.getElementsByClassName("img2")[0].setAttribute("src", randomDiceImage2); 
+
+if (randomDiceImage1 > randomDiceImage2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+} else if  (randomDiceImage1 < randomDiceImage2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+} else if  (randomDiceImage1 = randomDiceImage2) {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
