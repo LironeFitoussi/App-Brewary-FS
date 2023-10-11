27c0c0024f3bb5da8b78b6c10193e3e3354b8beb
LironeFitoussi <lironefit@icloud.com>
2023-10-11 13:14:54 +0300

    upload

--- Section 24 - Express.js with Node.js/203/3.2 HTTP Requests/index.js
+++ Section 24 - Express.js with Node.js/203/3.2 HTTP Requests/index.js
@@ -1,6 +1,28 @@
 import express from "express";
 const app = express()
 const port = 3000
+
+app.get("/", (req, res) => {
+    console.log(req.rawHeaders);
+    res.send("<h1>Hello<h1>")
+})
+
+app.get("/about", (req, res) => {
+    console.log(req.rawHeaders);
+    res.send(`
+        <h1>About Me</h1>
+        <p>My Name is Lirone</p>
+    `)
+})
+
+app.get("/contact", (req, res) => {
+    console.log(req.rawHeaders);
+    res.send(`
+        <h1>Contact Me:<h1>
+        <p>My Phone number is: +972585109829</p>
+    `)
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 })
--- Section 24 - Express.js with Node.js/203/Server at 2000/index.js
+++ Section 24 - Express.js with Node.js/203/Server at 2000/index.js
@@ -1,9 +1,7 @@
 import express from "express"
 const app = express()
 const port = 3000
-app.get("/", () => {
-    console.log("This is index page");
-})
+
 app.listen(3000,() => {
     console.log(`Server is runing at port ${port}`);
 })
