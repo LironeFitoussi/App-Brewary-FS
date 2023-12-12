91521a0d57de23dbcf4bd2ff791677974812de86
LironeFitoussi <lironefit@icloud.com>
2023-12-12 08:10:05 +0200

    308 Done

--- Section 36 - React.js/308/src/components/App.jsx
+++ Section 36 - React.js/308/src/components/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import Login from "./Login";
+
+let isLoggendIn = false;
+
+// const currentTime = new Date(2023, 12, 12, 9).getHours();
+const currentTime = new Date(2023, 12, 12, 18).getHours();
+
+// console.log(currentTime);
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {/* {isLoggendIn? <h1>Hello</h1> : <Login />} */}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
     </div>
   );
 }
 
 export default App;
+
+// ("EXPERESSION1" && "EXPERESSION2") // Output: Boolean 
+var x = 5;
+if (x > 3 && x < 7) {
+  console.log(true);
+}
