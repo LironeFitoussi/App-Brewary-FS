cea6e12b32834d5f4e2f25449614f44cd3ea7899
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2023-12-24 12:32:37 +0200

    PostgreSQL progress

---
+++ Section 33 - PostgreSQL/250/base table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+
+
+CREATE TABLE friends (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT, 
+    is_cool BOOLEAN
+)
+
---
+++ Section 33 - PostgreSQL/252/falgsTableCreate.sql
@@ -0,0 +1,5 @@
+CREATE TABLE flags (
+	id SERIAL PRIMARY KEY,
+    name TEXT,
+    flag TEXT
+) 
