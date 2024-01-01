e160d32d0698a8a80228c8f3099be15f9f1c48fe
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-01 21:15:14 +0200

    Section Completed

--- Section 33 - PostgreSQL/263/8.6 Permalist Project/index.js
+++ Section 33 - PostgreSQL/263/8.6 Permalist Project/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import ip from "ip"
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "Nveurvtjh147",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,7 +23,20 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const ipAddress = ip.address();
+ console.log(`${ipAddress} is Connected.`);
+
+  try {
+   const result  = await db.query("SELECT * FROM items ORDER BY id ASC")
+   items = result.rows
+  console.log(items);
+
+  }
+  catch(err) {
+    console.log(err);
+  }
+
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
@@ -21,13 +45,42 @@ app.get("/", (req, res) => {
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  // items.push({ title: item });
+  try {
+    db.query('INSERT INTO items (title) VALUES ($1);', [item])
+    res.redirect('/')
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  const updatedItem = {id: req.body.updatedItemId, title: req.body.updatedItemTitle}
+  console.log(updatedItem);
 
-app.post("/delete", (req, res) => {});
+  try {
+    db.query('UPDATE items SET title = ($1) WHERE id = ($2)', [updatedItem.title, updatedItem.id])
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+  
+});
+
+app.post("/delete", (req, res) => {
+  try {
+    db.query('DELETE FROM items WHERE id = ($1)', [req.body.deleteItemId])
+    res.redirect('/')
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+// Catch-all route for handling requests to non-existing routes
+app.get("*", (req, res) => {
+  res.status(404).render("404.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
